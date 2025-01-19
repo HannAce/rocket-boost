f5c5dc6860dd6b27688f27b22d86b460d978f4ea
Hannah Fisher <hannahtfisher@gmail.com>
2025-01-19 19:49:15 +0000

    Added player thrust and rotation for movement. Setup some prefabs

--- Assets/MyAssets/Scripts/Movement.cs
+++ Assets/MyAssets/Scripts/Movement.cs
@@ -4,18 +4,43 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody m_rb;
     [SerializeField] private InputAction m_thrust;
+    [SerializeField] private InputAction m_rotation;
+    
+    [SerializeField] private float m_thrustSpeed;
 
     private void OnEnable()
     {
         m_thrust.Enable();
+        m_rotation.Enable();
     }
 
-    private void Update()
+    private void FixedUpdate()
+    {
+        PlayerThrust();
+        PlayerRotation();
+    }
+    
+    private void PlayerThrust()
     {
         if (m_thrust.IsPressed())
         {
-            Debug.Log("Thrust Pressed");
+            m_rb.AddRelativeForce(Vector3.up * m_thrustSpeed * Time.fixedDeltaTime);
+        }
+    }
+    
+    private void PlayerRotation()
+    {
+        if (m_rotation.IsPressed())
+        {
+            float rotationInput = m_rotation.ReadValue<float>();
+            if (rotationInput != 0)
+            {
+                transform.Rotate(0, 0, -rotationInput); 
+            }
+            
+            Debug.Log(rotationInput);
         }
     }
 }
